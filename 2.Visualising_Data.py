d222b8a718087842496c3b690a1f39234625b765
Ruslan Brilenkov <ruslan.brilenkov@gmail.com>
2020-05-21 00:32:03 +0200

    Added mode codes
    I add 2.Visualization code, 3.K-means clustering code.
    The second one contains the "meat of the algorithm". The description is commented in the code.
    With a nice visualization, we can see step by step, how this algorithm works.

---
+++ 2.Visualising_Data.py
@@ -0,0 +1,60 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def ReadData():
+	
+	data = np.loadtxt("Data/K-means-Data.txt", delimiter=',')
+	labels = np.loadtxt("Data/K-means-Data-labels.txt", delimiter=',')
+	
+	return data, labels
+
+def MakePlot(data, labels):
+	
+	# Checking if folder names "Plots/" is present / create one if not.
+	folderPlotsName = "Plots/"
+	if not os.path.exists(folderPlotsName):
+		os.makedirs(folderPlotsName)
+	
+	plt.figure()
+	plt.scatter(data[:,0], data[:,1], c=labels, cmap='viridis')
+	plt.xlim(-20,20)
+	plt.ylim(-20,20)
+	plt.savefig(folderPlotsName+'random_blobs.png')
+
+def Create_More_Plots(data, labels):
+	
+	# Checking if folder names "Plots/" is present / create one if not.
+	folderPlotsName = "Plots/"
+	if not os.path.exists(folderPlotsName):
+		os.makedirs(folderPlotsName)
+	
+	# create a bit more advanced scatter plot than previous method
+	plt.figure(figsize=[14,12])
+	plt.scatter(data[:,0], data[:,1], c=labels, cmap='viridis', marker='+', s=500)
+	plt.xlim(-20,20)
+	plt.ylim(-20,20)
+	plt.xlabel('feature 1', fontsize=18)
+	plt.ylabel('feature 2', fontsize=18)
+	plt.savefig(folderPlotsName+"data_colored.png", bbox_inches='tight', overwrite=True)
+
+	# create another scatter plot
+	plt.figure(figsize=[14,12])
+	plt.scatter(data[:,0], data[:,1], color='black', marker='+', s=500)
+	plt.xlim(-20,20)
+	plt.ylim(-20,20)
+	plt.xlabel('feature 1', fontsize=18)
+	plt.ylabel('feature 2', fontsize=18)
+	plt.savefig(folderPlotsName+"data_uncolored.png", bbox_inches='tight', overwrite=True)
+
+if (__name__=="__main__"):
+	
+	# reading data and their labels
+	Mydata, Mylabels = ReadData()
+	
+	MakePlot(Mydata, Mylabels)
+	
+	# If you would like a bit more advanced plotting 
+	# with axes labels, 
+	Create_More_Plots(Mydata, Mylabels)
---
+++ 3.K_means.py
@@ -0,0 +1,161 @@
+
+__author__ = "Ruslan Brilenkov"
+__copyright__ = "Copyright 2020, Unsupervised ML in Python from scratch project"
+__credits__ = ["Ruslan Brilenkov"]
+__email__ = "ruslan.brilenkov@gmail.com"
+
+
+class KMeans(object):
+	"""
+	Implementation of K-Means Clustering
+	"""
+
+	# __init__ method will run authomatically upon calling the class
+	# we want import a number of clusters which we want to fit to our data
+	def __init__(self, k):
+		self.k = k
+    
+	def ReadData(self):
+
+		data = np.loadtxt("Data/K-means-Data.txt", delimiter=',')
+		#labels = np.loadtxt("Data/K-means-Data-labels.txt", delimiter=',')
+
+		return data#, labels
+	
+	'''
+	def plot(self, X):
+		
+		# randomly creating the cluster centers
+		self.centers = X[np.random.randint(X.shape[0], size=self.k)]
+		
+		plt.figure(figsize=[14,12])        
+		
+		# plotting the data points as blue plusses
+		plt.plot(X[:,0], X[:,1], 'b+', markersize=30, markeredgewidth = 5)
+				# plotting randomly generated K-centers as red circles
+		plt.plot(self.centers[:,0], self.centers[:,1], 'ro', markersize = 32)
+		plt.xlabel('feature 1', fontsize=18)
+		plt.ylabel('feature 2', fontsize=18)
+		plt.show()
+	'''
+     
+	def ColorPlotInitialize(self, X):
+		
+		'''
+		Initializing k-clusters at random positions and assigning 
+		the nearest data points to the closest cluster center
+		'''		
+		
+		# Checking if folder names "Plots/" is present / create one if not.
+		folderPlotsName = "Plots/"
+		if not os.path.exists(folderPlotsName):
+			os.makedirs(folderPlotsName)
+
+		# creating colors to paint data points
+		# !!! This part should be improved to the case of arbitrary number of clusters
+		colors = ["red", "blue", "green", "orange", "violet", "brown"]
+
+		# randomly assigning the positions of cluster centers
+		self.centers = X[np.random.randint(X.shape[0], size=self.k)]
+
+		# calculating the distances from each cluster center to every point in the dataset
+		distances = np.sqrt(np.sum((X - self.centers[:, np.newaxis]) ** 2, axis=2))
+		# chosing the closest data points
+		closestClusters = np.argmin(distances, axis=0)
+		
+		# Visualizing the initial random cluster distribution
+		plt.figure(figsize=[14,12])        
+
+		# plotting data ploints with the colors relative to which cluster they are assigned to.
+		# and plotting randomly generated K-centers as red circles with relative colors
+		for j in range(self.k):
+			plt.scatter(X[:,0][closestClusters == j], X[:,1][closestClusters == j], color=colors[j], marker='+', s=500)
+			plt.plot(self.centers[j,0], self.centers[j,1], 'o', c = colors[j], markersize = 32)
+
+		plt.xlabel('feature 1', fontsize=18)
+		plt.ylabel('feature 2', fontsize=18)
+		#plt.show()
+		plt.savefig(folderPlotsName+"k_means_initialize_color.png", bbox_inches='tight', overwrite=True)
+
+	def MeatOfProgram(self, X):
+		
+		'''
+		1) Initializing k-clusters at random positions and assigning 
+		the nearest data points to the closest cluster center
+		
+		2) Determining the mean of the 
+		'''		
+		
+		# Checking if folder names "Plots/" is present / create one if not.
+		folderPlotsName = "Plots/"
+		if not os.path.exists(folderPlotsName):
+			os.makedirs(folderPlotsName)
+
+		# Creating another folder - to contain the plots of the algorithm
+		folderAlgorithmName = "K_means_Plots/"
+		if not os.path.exists(folderPlotsName+folderAlgorithmName):
+			os.makedirs(folderPlotsName+folderAlgorithmName)
+			
+		# creating colors to paint data points
+		# !!! This part should be improved to the case of arbitrary number of clusters
+		colors = ["red", "blue", "green", "orange", "violet", "brown"]
+
+		# randomly assigning the positions of cluster centers
+		self.centers = X[np.random.randint(X.shape[0], size=self.k)]
+
+		# There is a meat of the algorithm - for this we will have to loop this part until 
+		# the algorithm converges
+		        
+		
+		MyIndex = 0
+		
+		while True:
+			# calculating the distances from each cluster center to every point in the dataset
+			distances = np.sqrt(np.sum((X - self.centers[:, np.newaxis]) ** 2, axis=2))
+			# chosing the closest data points
+			closestClusters = np.argmin(distances, axis=0)
+
+			# assigning new k clusters based on the mean of the closest centers
+			newCenters = np.array([np.mean(X[closestClusters == c], axis=0) for c in range(self.k)])
+			
+			# To break the loop in case of convergence 
+			if np.all(self.centers - newCenters < 1e-5):
+				break
+			self.centers = newCenters
+			
+			# Visualizing the initial random cluster distribution
+			plt.figure(figsize=[14,12])
+
+			# plotting data ploints with the colors relative to which cluster they are assigned to.
+			# and plotting randomly generated K-centers as red circles with relative colors
+			for j in range(self.k):
+				plt.scatter(X[:,0][closestClusters == j], X[:,1][closestClusters == j], color=colors[j], marker='+', s=500)
+				plt.plot(self.centers[j,0], self.centers[j,1], 'o', c = colors[j], markersize = 32)
+
+			plt.xlabel('feature 1', fontsize=18)
+			plt.ylabel('feature 2', fontsize=18)
+			#plt.show()
+			plt.savefig(folderPlotsName+folderAlgorithmName+"k_means_step"+str(MyIndex)+".png", bbox_inches='tight', overwrite=True)
+			MyIndex += 1
+		
+# Let's try to call the class functions
+if (__name__=='__main__'):
+    
+	import numpy as np
+	import matplotlib.pyplot as plt
+
+	import random
+	import os
+	
+	# calling the class methods
+	kmeans = KMeans(k=5)
+	
+	# reading data and their labels (by calling the method inside the class)
+	Mydata = kmeans.ReadData()
+	
+	# Initializing k-clusters at random positions and assigning data to the nearest cluster
+	# (by calling the method inside the class)
+	kmeans.ColorPlotInitialize(Mydata)
+	
+	# The "meat" of teh lagorithm - reassigning the cluster centers based on the mean of all assigned points
+	kmeans.MeatOfProgram(Mydata)
